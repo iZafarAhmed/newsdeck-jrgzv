6bd1e161fd79d76a21bc5ad012f68ae9b35884ec
Studies <omgamazingpics@gmail.com>
2025-10-20 06:29:20 +0000

    404 This page could not be found.

---
+++ src/app/golf-news/page.tsx
@@ -0,0 +1,19 @@
+
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+import { GolfNewsClient } from "./_components/golf-news-client";
+import { golfNewsSources } from "@/data/golf-news-sources";
+
+export const revalidate = 3600; // Revalidate every hour
+
+export default function GolfNewsPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-card">
+      <Header />
+      <div className="flex-grow">
+        <GolfNewsClient sources={golfNewsSources} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
