f51f496c853e4e7478a25177b7d372fa5eff29ba
Studies <omgamazingpics@gmail.com>
2025-10-22 02:32:38 +0000

    Indiana News Websites provide top visited 50

---
+++ src/app/indiana-news/page.tsx
@@ -0,0 +1,19 @@
+
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+import { IndianaNewsClient } from "./_components/indiana-news-client";
+import { indianaNewsSources } from "@/data/indiana-news-sources";
+
+export const revalidate = 3600; // Revalidate every hour
+
+export default function IndianaNewsPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white dark:bg-background">
+      <Header />
+      <div className="flex-grow">
+        <IndianaNewsClient sources={indianaNewsSources} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
